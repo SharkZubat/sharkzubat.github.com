2e4e308aa45a3d487497687b4710abd170369bcf
Shark Zubat <akula.puppet.zaba123@gmail.com>
2025-01-06 14:46:19 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
 document.addEventListener('DOMContentLoaded', (event) => {
     const sections = document.querySelectorAll('section');
+    
+    function updateTitle() {
+        const hash = window.location.hash;
+        if (hash) {
+            const section = document.querySelector(hash);
+            if (section) {
+                document.title = section.querySelector('h2').innerText + " - Shark Zubat's Website";
+            }
+        }
+    }
+    
     sections.forEach(section => {
         section.addEventListener('click', () => {
             document.title = section.querySelector('h2').innerText + " - Shark Zubat's Website";
         });
     });
+    
+    window.addEventListener('hashchange', updateTitle);
+    
+    // Update the title on initial load
+    updateTitle();
 });
